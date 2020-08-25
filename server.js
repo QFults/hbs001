9931503d3c31fe35bc118f9dc017d48d94b74374
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-25 11:37:00 -0700

    code

--- server.js
+++ server.js
@@ -22,16 +22,23 @@ app.set('view engine', '.hbs')
 //   res.render('contact')
 // })
 
-const movie = {
-  title: 'Goodfellas',
-  poster: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffanart.tv%2Ffanart%2Fmovies%2F769%2Fmovieposter%2Fgoodfellas-5236f4eb565d5.jpg&f=1&nofb=1',
-  plot: `A young man grows up in the mob and works very hard to advance himself through the ranks. He enjoys his life of money
+const movies = [
+  {
+    title: 'Goodfellas',
+    poster: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffanart.tv%2Ffanart%2Fmovies%2F769%2Fmovieposter%2Fgoodfellas-5236f4eb565d5.jpg&f=1&nofb=1',
+    plot: `A young man grows up in the mob and works very hard to advance himself through the ranks. He enjoys his life of money
 and luxury, but is oblivious to the horror that he causes. A drug addiction and a few mistakes ultimately unravel his
 climb to the top. Based on the book "Wiseguy" by Nicholas Pileggi.`
-}
+  },
+  {
+    title: 'The Room',
+    poster: 'https://upload.wikimedia.org/wikipedia/en/e/e1/TheRoomMovie.jpg',
+    plot: `Johnny is a successful banker who lives happily in a San Francisco townhouse with his fiancée, Lisa. One day, inexplicably, she gets bored of him and decides to seduce Johnny's best friend, Mark. From there, nothing will be the same again.`
+  }
+]
 
 app.get('/movie', (req, res) => {
-  res.render('movie', movie)
+  res.render('movie', { movies })
 })
 
 app.listen(3000)
